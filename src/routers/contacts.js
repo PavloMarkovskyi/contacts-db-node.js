d2fcddb5e98e7f46b9f508d5e69b383fd8986198
Pavlo Markovskyi <pash199060@gmail.com>
2025-07-31 20:58:40 +0300

    add validation & pagination

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,10 +7,33 @@ import {
   updateContactByIdController,
   deleteContactByIdController,
 } from '../controllers/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 const router = Router();
 router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactByIdController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactByIdController),
+);
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactByIdController),
+);
 export default router;
