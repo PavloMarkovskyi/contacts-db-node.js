ab9ebaa6f15f661343c8f63dada6e0e8fea154ac
Pavlo Markovskyi <pash199060@gmail.com>
2025-07-31 23:32:35 +0300

    add sort & filter

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,10 +7,20 @@ import {
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getContactsController = async (req, res) => {
+  const { sortBy, sortOrder } = parseSortParams(req.query);
   const { page, perPage } = parsePaginationParams(req.query);
-  const contacts = await getAllContacts({ page, perPage });
+  const filter = parseFilterParams(req.query);
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
